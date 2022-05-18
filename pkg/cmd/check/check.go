691acaf124128aa7a4699b880a822e75c71e482a
Skisocks <jaa.skeoch@gmail.com>
2022-05-18 09:05:55 +0100

    refactor: split Run() & improve variable naming

--- pkg/cmd/check/check.go
+++ pkg/cmd/check/check.go
@@ -13,6 +13,7 @@ import (
 	"github.com/jenkins-x/jx-logging/v3/pkg/log"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"strings"
 )
 
@@ -24,30 +25,29 @@ type Options struct {
 	GitClient     gitclient.Interface
 	CommandRunner cmdrunner.CommandRunner
 
-	Namespace       string
-	LatestCommitSha string
-	CurrentRevSha   string
-	RepoDir         string
+	firstSha  string
+	latestSha string
+	dir       string
 }
 
 var (
 	cmdLong = templates.LongDesc(`
-		Checks whether the commit messages in a pull request follow Conventional Commits.
+		Checks whether the commit messages in a pull request follow the Conventional Commits specification
 `)
 
 	cmdExample = templates.Examples(`
 		jx-semanticcheck check 
 `)
 
-	ConventionalCommitTitles = []string{"feat", "fix", "perf", "refactor", "docs", "test", "revert", "style", "chore", "build"}
+	ConventionalCommitTypes = []string{"feat", "fix", "perf", "refactor", "docs", "test", "revert", "style", "chore", "build"}
 )
 
+// NewCmdCheckSemantics creates a command object for the command
 func NewCmdCheckSemantics() (*cobra.Command, *Options) {
 	o := &Options{}
 	cmd := &cobra.Command{
-		Use:   "check",
-		Short: "Checks for whether the commits are conventional",
-		// Aliases: []string{"changelog", "changes", "publish"},
+		Use:     "check",
+		Short:   "Checks for whether the commits in a PR are Conventional Commits",
 		Long:    cmdLong,
 		Example: cmdExample,
 		Run: func(cmd *cobra.Command, args []string) {
@@ -56,13 +56,54 @@ func NewCmdCheckSemantics() (*cobra.Command, *Options) {
 		},
 	}
 	o.ScmFactory.DiscoverFromGit = true
-	cmd.Flags().StringVarP(&o.CurrentRevSha, "first-sha", "p", "", "the first commit SHA of this revision")
-	cmd.Flags().StringVarP(&o.LatestCommitSha, "latest-sha", "", "", "the latest commit SHA")
-	cmd.Flags().StringVarP(&o.RepoDir, "repo-dir", "", "", "the directory of the git repository")
+	cmd.Flags().StringVarP(&o.firstSha, "first-sha", "", "", "the first commit SHA to check")
+	cmd.Flags().StringVarP(&o.latestSha, "latest-sha", "", "", "the last commit SHA to check")
+	cmd.Flags().StringVarP(&o.dir, "dir", "", "", "the directory of the repository")
 
 	return cmd, o
 }
 
+func (o *Options) Run() error {
+	err := o.Validate()
+	if err != nil {
+		return errors.Wrapf(err, "failed to validate")
+	}
+
+	commits, err := o.GetCurrentRevCommits()
+	if err != nil {
+		return errors.Wrapf(err, "failed to get commits")
+	}
+
+	if strings.HasPrefix((*commits)[0].Message, "release ") {
+		// Ignore release commit
+		*commits = (*commits)[1:]
+	}
+
+	var failedCommits int
+	for _, commit := range *commits {
+		var terminalMessage string
+		indicator := "✓"
+
+		if !IsCommitConventional(commit.Message) {
+			indicator = "x"
+			terminalMessage = commit.Message
+			failedCommits++
+		}
+
+		log.Logger().Infof("---  Commit | %s --- %s\n"+
+			"%s",
+			commit.Hash, indicator, terminalMessage)
+	}
+
+	if failedCommits > 0 {
+		return fmt.Errorf("%d commit(s) did not follow https://conventionalcommits.org/", failedCommits)
+	}
+
+	log.Logger().Infof("\nAll commits follow https://conventionalcommits.org/")
+	return nil
+}
+
+// Validate checks that all the variables required to run are present
 func (o *Options) Validate() error {
 	err := o.BaseOptions.Validate()
 	if err != nil {
@@ -74,95 +115,52 @@ func (o *Options) Validate() error {
 		return errors.Wrapf(err, "failed to discover git repository")
 	}
 
+	if o.dir == "" {
+		o.dir = o.ScmFactory.Dir
+	}
+
+	if o.GitClient == nil {
+		o.GitClient = cli.NewCLIClient("", o.CommandRunner)
+	}
 	return nil
 }
 
-func (o *Options) Run() error {
-	err := o.Validate()
-	if err != nil {
-		return errors.Wrapf(err, "failed to validate")
-	}
-
-	o.CommandRunner = cmdrunner.QuietCommandRunner
-
-	dir := o.RepoDir
-	if dir == "" {
-		dir = o.ScmFactory.Dir
-	}
-
-	if o.CurrentRevSha == "" {
-		o.CurrentRevSha, _, err = gitclient.GetCommitPointedToByLatestTag(o.git(), dir)
+// GetCurrentRevCommits returns the commits since the last revision
+func (o *Options) GetCurrentRevCommits() (commits *[]object.Commit, err error) {
+	if o.firstSha == "" {
+		o.firstSha, _, err = gitclient.GetCommitPointedToByLatestTag(o.GitClient, o.dir)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	if o.LatestCommitSha == "" {
-		o.LatestCommitSha, err = gitclient.GetLatestCommitSha(o.git(), dir)
+	if o.latestSha == "" {
+		o.latestSha, err = gitclient.GetLatestCommitSha(o.GitClient, o.dir)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-
-	commits, err := chgit.FetchCommits(dir, o.CurrentRevSha, o.LatestCommitSha)
-	if err != nil {
-		return err
-	}
-
-	commitSlice := *commits
-	if strings.HasPrefix(commitSlice[0].Message, "release ") {
-		// remove the release commit from the log
-		commitSlice = commitSlice[1:]
-	}
-
-	var failedCommitCount int
-	for _, commit := range commitSlice {
-		var terminalMessage string
-		passMark := "✓"
-
-		if !IsCommitSemantic(commit.Message) {
-			passMark = "x"
-			terminalMessage = commit.Message
-			failedCommitCount++
-		}
-
-		log.Logger().Infof("---  Commit | %s --- %s\n"+
-			"%s",
-			commit.Hash, passMark, terminalMessage)
-	}
-
-	if failedCommitCount > 0 {
-		return fmt.Errorf("%d commit(s) did not follow https://conventionalcommits.org/", failedCommitCount)
-	}
-	log.Logger().Infof("\nAll commits follow https://conventionalcommits.org/")
-	return nil
+	return chgit.FetchCommits(o.dir, o.firstSha, o.latestSha)
 }
 
-// IsCommitSemantic checks whether the commit message follow the conventions set out in
-// Conventional Commits
-func IsCommitSemantic(commitMessage string) bool {
+// IsCommitConventional checks whether a commit message follows the conventions by comparing its prefix
+// to those in ConventionalCommitTypes
+func IsCommitConventional(commitMessage string) bool {
 	commitMessage = strings.TrimSpace(strings.ToLower(commitMessage))
+
+	// Ignore revert or merge commits
+	if strings.Contains(commitMessage, "revert") || strings.Contains(commitMessage, "merge") {
+		return true
+	}
 
 	idx := strings.Index(commitMessage, ":")
 	if idx > 0 {
-		commitTitle := commitMessage[0:idx]
-		for _, semanticTitle := range ConventionalCommitTitles {
-			if strings.HasPrefix(commitTitle, semanticTitle) {
+		commitType := commitMessage[0:idx]
+		for _, conventionalType := range ConventionalCommitTypes {
+			if strings.HasPrefix(commitType, conventionalType) {
 				return true
 			}
 		}
 	}
-
-	// Check that the commit isn't a revert or merge
-	if strings.Contains(commitMessage, "revert") || strings.Contains(commitMessage, "merge") {
-		return true
-	}
 	return false
-}
-
-func (o *Options) git() gitclient.Interface {
-	if o.GitClient == nil {
-		o.GitClient = cli.NewCLIClient("", o.CommandRunner)
-	}
-	return o.GitClient
 }
--- pkg/cmd/check/check_test.go
+++ pkg/cmd/check/check_test.go
@@ -80,7 +80,7 @@ func TestIsCommitSemantic(t *testing.T) {
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			actualResult := IsCommitSemantic(tt.commitMessage)
+			actualResult := IsCommitConventional(tt.commitMessage)
 			assert.Equal(t, tt.expectedResult, actualResult)
 		})
 	}
